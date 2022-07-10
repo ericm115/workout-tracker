23433a87cd51c8fd190f6ee3ecfba73938809bb0
eric <ericm115@gmail.com>
2022-07-10 16:02:34 -0400

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {pokemon} from "../stores/pokestore";
+    import PokemanCard from "../components/pokemanCard.svelte"
+</script>
+<svelte:head>
+    <title>Workout Tracker</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit</h1>
+<div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
+{#each $pokemon as pokeman}
+    <PokemanCard pokeman={pokeman}/>
+{/each}
+</div>
