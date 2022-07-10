be9034dc2832cc715467e9ddfa2104f26d529b8d
eric <ericm115@gmail.com>
2022-07-10 16:32:42 -0400

    reset

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,12 @@
 <script>
     import {pokemon} from "../stores/pokestore";
     import PokemanCard from "../components/pokemanCard.svelte"
-import { altoids } from "src/stores/test";
 </script>
 <svelte:head>
     <title>Workout Tracker</title>
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit</h1>
 <div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
-<div>{$altoids}</div>
     {#each $pokemon as pokeman}
     <PokemanCard pokeman={pokeman}/>
 {/each}
