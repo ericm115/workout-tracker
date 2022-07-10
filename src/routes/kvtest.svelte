34d98b456b54bfd9754a41a05450b66ce12f46d3
eric <ericm115@gmail.com>
2022-07-10 17:08:41 -0400

    vsrgd

--- src/routes/kvtest.svelte
+++ src/routes/kvtest.svelte
@@ -5,4 +5,9 @@
     console.log(JSON.stringify(item))
   </script>
   
-  <h1>{item}</h1>
+  <h1>KV Test</h1>
+  {#each item as i}
+    <p>Name: {i.name}</p>
+    <p>Phone: {i.phone}</p>
+    <p>Address: {i.address}</p>
+  {/each}
