805f86d7d4d0deb828d7b10a35e2e82e870c9ed8
eric <ericm115@gmail.com>
2022-07-10 17:16:00 -0400

    dng324

--- src/routes/kvtest.js
+++ src/routes/kvtest.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ request, platform }) {
   // `params.id` comes from [id].js
-  const item = await platform.env.KVNamespace.get("one");
+  const item = await platform.env.KVNamespace.get("one").json();
 
   if (item) {
     return {
