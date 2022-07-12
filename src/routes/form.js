a998cc86b14ea4e87b3d8d1124f1d73e7a90c72b
eric <ericm115@gmail.com>
2022-07-11 22:02:47 -0400

    q

--- src/routes/form.js
+++ src/routes/form.js
@@ -18,8 +18,8 @@ export async function get({ request, platform }) {
   };
 }
 
-export async function post({ req, platform }) {
-  let { name, phone, address, preData } = await req.json();
+export async function post({ request, platform }) {
+  let { name, phone, address, preData } = await request.json();
   preData = JSON.parse(preData);
   preData.push({ name: name, phone: phone, address: address });
   preData = JSON.stringify(preData);
